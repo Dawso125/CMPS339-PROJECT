e2ab8bbcfe4ccd84175758ab897360907e15754e
Dawso125 <dawson.davis@selu.edu>
2023-06-23 02:42:30 -0500

    Added Swagger to the project

--- CMPS339 PROJECT/Program.cs
+++ CMPS339 PROJECT/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +23,12 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html"); ;
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+    options.DocumentTitle = "My Swagger";
+});
 
 app.Run();
