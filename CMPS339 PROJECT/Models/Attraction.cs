82637af218938d6616eb3d36a3daeaed74626877
Dawso125 <dawson.davis@selu.edu>
2023-07-11 23:18:03 -0500

    Started on the Attraction Model, Controller, Services

---
+++ CMPS339 PROJECT/Models/Attraction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMPS339_PROJECT.Models
+{
+    public class Attraction
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("AmusementPark")]
+        public int ParkId { get; set; }
+        public Parks? Park { get; set; }
+    }
+
+    public class AttractionDto
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("AmusementPark")]
+        public int ParkId { get; set;}
+    }
+}
---
+++ CMPS339 PROJECT/Services/Implementations/AttractionsService.cs
@@ -0,0 +1,60 @@
+﻿using CMPS339_PROJECT.Models;
+using CMPS339_PROJECT.Services.Interfaces;
+using Dapper;
+using Microsoft.Extensions.Caching.Memory;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace CMPS339_PROJECT.Services.Implementations
+{
+    public class AttractionsService : IAttractionsService
+    {
+
+        private readonly ILogger<AttractionsService> _logger;
+        private readonly IMemoryCache _cache;
+
+        public AttractionsService(ILogger<AttractionsService> logger, IMemoryCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        public async Task<List<Attraction>> GetAllAsync()
+        {
+            List<Attraction> attractions = new();
+            using (IDbConnection connection = new SqlConnection(ConnectionService.ConnectionString))
+            {
+                connection.Open();
+
+                var attractionData = await connection.QueryAsync<Attraction>("SELECT * FROM Attractions");
+
+                attractions = attractionData.ToList();
+            }
+            return attractions;
+        }
+
+        public async Task<Attraction?> GetByIdAsync(int id)
+        {
+            List<Attraction> attractions = new();
+
+            using (IDbConnection connection = new SqlConnection(ConnectionService.ConnectionString))
+            {
+                connection.Open();
+
+                var attractionData = await connection.QueryAsync<Attraction>("SELECT * FROM Attractions WHERE Id = @Id", new { Id = id });
+                attractions = attractionData.ToList();
+
+            }
+            return attractions.FirstOrDefault();
+
+        }
+
+    }
+}
